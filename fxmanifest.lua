1519dfa4e2c6c9b4d19557398d9552e228d92f70
Notion-Roleplay <timberland16@hotmail.de>
2026-02-22 14:19:06 +0100

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,20 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'Notion-Roleplay © 2026'
+description 'Notion-Roleplay.de Combat Protection (Wasabi Compatible)'
+version '1.1.0'
+
+shared_scripts {
+    'config.lua'
+}
+
+client_scripts {
+    'client/client.lua'
+}
+
+server_scripts {
+    'server/server.lua'
+}
+
+dependency 'qb-core'
