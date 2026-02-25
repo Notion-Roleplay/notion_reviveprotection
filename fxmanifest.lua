a990329a60f64464f17f54bc5cd1ad131ccb19e4
Notion-Roleplay <timberland16@hotmail.de>
2026-02-25 16:35:48 +0100

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -3,7 +3,7 @@ game 'gta5'
 
 author 'Notion-Roleplay © 2026'
 description 'Notion-Roleplay.de Combat Protection (Wasabi Compatible)'
-version '1.1.0'
+version '1.1.1'
 
 shared_scripts {
     'config.lua'
